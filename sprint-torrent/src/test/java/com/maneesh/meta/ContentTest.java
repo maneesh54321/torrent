7561eb9ae2b04ab8be8ca93e9b3c5b02f61bb459
Maneesh Singh <maneesh54321@gmail.com>
2024-06-16 11:17:29 +0530

    Setup jacoco

---
+++ sprint-torrent/src/test/java/com/maneesh/meta/ContentTest.java
@@ -0,0 +1,20 @@
+package com.maneesh.meta;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContentTest {
+
+    @Test
+    void getDownloadFileByPieceIndex() {
+    }
+
+    @Test
+    void getRootDirectoryName() {
+    }
+
+    @Test
+    void getDownloadFiles() {
+    }
+}
